a1ef3dcbeacf43d01ceb28d000cd74da0c0c39d3
huaxuelin <hualin0110@163.com>
2019-05-23 14:51:12 +0800

    修改rename和discover

--- linux-operation/rename_v0.1.sh
+++ linux-operation/rename_v0.1.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
+##
+##传入hostname的前缀
+read -p "请输入hostname的前缀: " pre_name
+#如果不想手动输入，可以在下面的pre_name直接写要的值
+#pre_name=$1
 host_version=$(rpm -q centos-release |cut -d - -f3)
 echo "$host_version"
 if [ "$host_version" = "7" ]
 then
-	IN_Face=`route -n |awk '{if($4~/U/){print $8}}'|head -n 1`
-	ipfull=`ip a|grep -B1 -C1 -w "$IN_Face" | grep -w 'inet' |awk '{print $2}' |awk -F'/' '{print $1}'`
-	ipmachine=`echo $ipfull|awk -F. '{print $3"-"$4}'`
-	machine=$1-$ipmachine
-#	sed -i 's/\(^HOSTNAME=\).*/\1'$machine'/g' /etc/sysconfig/network
-	if [[ -z `grep "$machine" /etc/hosts` ]]
-	then
-        	echo $ipfull $machine 
-        	echo $ipfull $machine >> /etc/hosts
-	        hostname $machine
-		>/etc/hostname
-		echo "$machine" >>/etc/hostname
-	else
-	        sed -i  /"$ipfull"/'c '"$ipfull $machine"'' /etc/hosts
-	fi
-	sed -i '/#UseDNS yes/a\UseDNS no' /etc/ssh/sshd_config
-	sed -i 's/GSSAPIAuthentication yes/GSSAPIAuthentication no/g' /etc/ssh/ssh_config
-	systemctl restart sshd.service
+        IN_Face=`route -n |awk '{if($4~/U/){print $8}}'|head -n 1`
+        ipfull=`ip a|grep -B1 -C1 -w "$IN_Face" | grep -w 'inet' |awk '{print $2}' |awk -F'/' '{print $1}'`
+        ipmachine=`echo $ipfull|awk -F. '{print $3"-"$4}'`
+        machine=$pre_name-$ipmachine #if want user-defined hostname ,you can use this line and run by one parameter
+#       machine=zqsign-$ipmachine
+#       sed -i 's/\(^HOSTNAME=\).*/\1'$machine'/g' /etc/sysconfig/network
+        if [[ -z `grep "$machine" /etc/hosts` ]]
+        then
+                echo $ipfull $machine
+                echo $ipfull $machine >> /etc/hosts
+                hostname $machine
+                >/etc/hostname
+                echo "$machine" >>/etc/hostname
+        else
+                sed -i  /"$ipfull"/'c '"$ipfull $machine"'' /etc/hosts
+        fi
+        sed -i '/#UseDNS yes/a\UseDNS no' /etc/ssh/sshd_config
+        sed -i 's/GSSAPIAuthentication yes/GSSAPIAuthentication no/g' /etc/ssh/ssh_config
+        systemctl restart sshd.service
 elif [ "$host_version" = "6" ]
 then
         IN_Face=`route -n |awk '{if($4~/UG/){print $8}}'|head -n 1`
         ipfull=`ip a|grep -B1 -C1 -w "$IN_Face" | grep -w 'inet' |awk '{print $2}' |awk -F'/' '{print $1}'`
         ipmachine=`echo $ipfull|awk -F. '{print $3"-"$4}'`
-        machine=$1-$ipmachine
+        machine=$pre_name-$ipmachine  #if want user-defined hostname ,you can use this line and run by one parameter
+        #machine=zqsign-$ipmachine
         sed -i 's/\(^HOSTNAME=\).*/\1'$machine'/g' /etc/sysconfig/network
         if [[ -z `grep "$machine" /etc/hosts` ]]
         then
@@ -39,4 +46,4 @@ then
         sed -i 's/GSSAPIAuthentication yes/GSSAPIAuthentication no/g' /etc/ssh/ssh_config
         /etc/init.d/sshd restart
 fi
-
+/bin/rm -rf rename_v0.1.sh
--- zabbix_scripts/discoverjavaport.sh
+++ zabbix_scripts/discoverjavaport.sh
@@ -1,9 +1,9 @@
-
 #!/bin/bash
 ##exclude_port这些进程是不需要的，如果需要屏蔽检测某些进程的端口，只需要在下面|grep CloudResetPwdUpdateAgent的后面，在添加|grep -v xxxx 即可。
 exclude_port=$(ps -ef |grep java |grep CloudResetPwdUpdateAgent |awk '{ print $2 }'|xargs |sed 's/ /\|/g')
 ##将需要的java端口检索出来。添加egrep -v 是为了屏蔽某些不需要的进程，所有的端口号，都会在下面的这个变量里面，但是将所有的端口取出来需要使用:${portarray[@]}
-portarray=(`sudo /bin/netstat -tnlp|egrep -i java|egrep -v $exclude_port|awk {'print $4'}|awk -F':' '{if ($NF~/^[0-9]*$/) print $NF}'|sort|uniq`)
+portarray=(`/bin/netstat -tnlp|egrep -i java|egrep -v $exclude_port|awk {'print $4'}|awk -F':' '{if ($NF~/^[0-9]*$/) print $NF}'|sort|uniq`)
+#下面可以加上sudo的使用方法，不过centos好像直接root权限，ubuntu需要设置zabbix的启动权限。
 #portarray=(`sudo /bin/netstat -tnlp|egrep -i java|awk {'print $4'}|awk -F':' '{if ($NF~/^[0-9]*$/) print $NF}'|sort|uniq`)
 length=${#portarray[@]}
 printf "{\n"
