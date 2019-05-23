67d64ff77eab5ecc473def1d1438761802713e82
郁唯 <hualin0110@163.com>
2019-05-23 16:21:00 +0800

    其实是修改了get_hetong.sh这个文件，可能其他的也被牵连了

--- linux-operation/onlyforzqsign/get_hetong .sh
+++ linux-operation/onlyforzqsign/get_hetong .sh
@@ -1,58 +1,33 @@
 #!/bin/bash
 
-
-downloadpath="/root/huxin/downloadpdf"
-mv $downloadpath $downloadpath$(date +%Y-%m-%d)
-mkdir -pv $downloadpath
-
-filelists="
-group1/M01/5A/37/wKgBEFxvc-SAASuQAADoe9Ugvz4442.pdf
+require_filelists="
+group1/M00/2E/52/wKgBz1zmUdCAYf6oAC93m3Laxk8428.zip
 "
 
-for i in $filelists;
-
-do
-#echo $i
-#filepath=`awk -F "\/" '{ print $2\/$3\/$4}'`
-#groupname=`echo $i |awk -F "M00" '{ print $1 }'`
-groupname=`echo $i |awk -F "M01" '{ print $1 }'`
-#filepath=`echo $i |awk -F "M00" '{ print $2 }'`
-filepath=`echo $i |awk -F "M01" '{ print $2 }'`
-
-#echo ${groupname%*/}
-#echo $filepath
-case "$groupname" in
-        group1/)
-		rsync -aP  -e "ssh -o StrictHostKeyChecking=no" root@192.168.0.25:/home1/fastdfs/storage/data$filepath  $downloadpath/
-                echo "Successfully download the file in group1 "
-                ;;
-        group2/)
-		rsync -aP  -e "ssh -o StrictHostKeyChecking=no"  root@192.168.1.14:/home1/fastdfs/storage/data$filepath  $downloadpath/
-                echo "Successfully download the file in group2 "
-                ;;
-        group3/)
-		rsync -aP  -e "ssh -o StrictHostKeyChecking=no"  root@192.168.0.231:/home1/fastdfs/storage/data$filepath  $downloadpath/
-                echo "Successfully download the file in group3 "
-                ;;
-        group4/)
-		rsync -aP  -e "ssh -o StrictHostKeyChecking=no"  root@192.168.1.95:/home1/fastdfs/storage/data$filepath  $downloadpath/
-                echo "Successfully download the file in group4 "
-                ;;
-        group5/)
-		rsync -aP  -e "ssh -o StrictHostKeyChecking=no"  root@192.168.0.60:/home/fastdfs/storage/data$filepath  $downloadpath/
-                echo "Successfully download the file in group4 "
-                ;;
-esac
-
-done
-if [ -f "/root/huxin/downloadpdf.zip" ];then
-echo "file is exit,will rm it"
-rm -rf /root/huxin/downloadpdf.zip
+workpath="/root/huxin"
+downloadpath="$workpath/downloadpdf"
+if [[ -d $downloadpath ]]; then
+	mv $downloadpath $workpath/downloadpath$(date +%Y-%m-%d)
+	mkdir -pv $downloadpath
+else
+	mkdir -pv $downloadpath
 fi
 
-#tar czfP /root/huxin/downloadpdf.tgz $downloadpath
-#echo "use zip command"
-#zip -r /root/huxin/downloadpdf.zip $downloadpath
+for i in $require_filelists;do
+	filename=$(echo $i|awk -F "/" '{ print $5 }')
+	/usr/bin/fdfs_download_file /etc/fdfs/client.conf $i $downloadpath/$filename
+
+done
+
+if [ -f "/root/huxin/downloadpdf.zip" ];then
+	echo "file is exit,and we will delete it"
+	rm -rf /root/huxin/downloadpdf.zip
+fi
+
+echo "will tar the downloadpath starting"
+tar czfP /root/huxin/downloadpdf.tgz $downloadpath
+echo "tar the download path is already successfully end"
+# zip -r /root/huxin/downloadpdf.zip $downloadpath
 
 #echo -e "您好：\n附件中是合同文件，请查收。" |mail -s "华雪林发来了合同文件" -a /root/huxin/downloadpdf.zip huxin@51signing.com
 #echo -e "您好：\n附件中是合同文件，请查收。" |mail -s "华雪林发来了合同文件" -a /root/huxin/downloadpdf.zip wangqi@51signing.com
