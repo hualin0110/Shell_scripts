a87f477451e14132384daaaa0f8246d6d8a45822
huaxuelin <hualin0110@163.com>
2020-04-14 00:26:25 +0800

    vol的监控

---
+++ VOL/push_status.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+'''
+# @FileName  :getOBS_Contractfile.py
+# @Date      :2020/4/9
+# @Author    :Xuelin_Hua
+'''
+import socket
+
+def get_host_ip():
+    """
+    查询本机ip地址
+    :return:
+    """
+    try:
+        s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8',80))
+        ip=s.getsockname()[0]
+    finally:
+        s.close()
+    return ip
+
+if __name__ == '__main__':
+    print(get_host_ip())
